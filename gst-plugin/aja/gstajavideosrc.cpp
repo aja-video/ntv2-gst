5e013551de1519f37eabd561003f7b2a73f313af
Sebastian Dröge <sebastian@centricular.com>
2018-04-25 12:47:00 +0300

    Update closed caption header file name

--- gst-plugin/aja/gstajavideosrc.cpp
+++ gst-plugin/aja/gstajavideosrc.cpp
@@ -24,7 +24,7 @@
 
 #include "gstajavideosrc.h"
 #include "gstajavideosrc.h"
-#include <gst/video/video-vbi.h>
+#include <gst/video/video-anc.h>
 
 
 GST_DEBUG_CATEGORY_STATIC (gst_aja_video_src_debug);
